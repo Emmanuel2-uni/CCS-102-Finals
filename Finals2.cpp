21f41361f735bcf6ebe9861fed1adc050ec460a8
Emmanuel <132662856+Emmanuel2-uni@users.noreply.github.com>
2023-05-07 19:25:14 +0800

    Update Finals2.cpp

--- Finals2.cpp
+++ Finals2.cpp
@@ -10,20 +10,46 @@
 //global variables
 bool program_run = true;
 int choice_menu;
+int employee_number;
+
+
+
 //int foo_mode;
 
+struct employee_Details{
+	std::string name;
+	std::string position;
+	std::string ID;
+	float salary;
+	
+	
+	float workTime;
+	float totalPayment;
+	std::string leaveStatus;
+	
+	employee_Details(std::string employee_Name, std::string employee_ID, std::string employee_Position, float employee_Hourly, float employee_workTime, float employee_totalPayment, std::string employee_leavestatus):
+		name(employee_Name),
+		ID(employee_ID),
+		position(employee_Position),
+		salary(employee_Hourly),
+		workTime(employee_workTime),
+		totalPayment(employee_totalPayment),
+		leaveStatus(employee_leavestatus)
+		{}
+	//These declarations allow the constructor to "construct" the object using the code below it
+	// This can also be seen as a *function* inside the struct/class. Where if you call for the object/class employee_Details, it will construct an object with those entries immediately.
+	// With the current parameters: name, position, salary, etc. -- whenever we use a proper STL function like emplace_back, we can update all of the parameters at once
+	// simultaneously without having to use backups or additional overhead/declarations.
+	// Benefit is that when this type (employee_Details) is used as a vector type. The entire structure is "stored" in 1 index, and we only need to enter the required data.
+	// Example: if the struct's member called "employee_name" is called i.e employee 
+	//{} the brackets at the end are needed.
+	
+	
+};
 
 //global vectors
-std::vector<std::string> employee_Names;
-//work details
-std::vector<std::string> employee_ID;
-std::vector<float> employee_hourlySalaries;
-std::vector<std::string> employee_Position;
-//work status
-std::vector<float> employee_totalPayment;
-std::vector<float> employee_Overtime;
-std::vector<int> employee_workTimes;
-std::vector<std::string> employee_leaveStatus;
+std::vector<employee_Details> employee;
+
 
 //new streams
 std::ofstream write_Text;
@@ -41,12 +67,19 @@ void list_Employee(std::string foo_mode);
 void clear_vectors();
 void input_employeeWorkDetails();
 int generate_ID();
+int read_params();
+
 
 
 int main(){
-	
+
 	do{
 		//main menu
+		employee_number = read_params();
+		read_Employee("Names");
+		list_Employee("Names");
+
+		std::cout << "\n\nNumber of Employees: " << employee_number << std::endl;
 		std::cout << "========Menu========\n\n";
 		std::cout << "1. List all employees." << std::endl;
 		std::cout << "2. Add an employee." << std::endl;
@@ -55,7 +88,7 @@ int main(){
 	
 		std::cout << "\n\n";
 		std::cout << "7. Exit." << std::endl;
-		std::cout << "0. Overwrite entire employee files" << std::endl;
+		std::cout << "0. Overwrite/Make new entire employee files" << std::endl;
 		
 		std::cout << "\n\nEnter choice: ";
 		std::cin >> choice_menu;	
@@ -130,14 +163,17 @@ void write_Employee(int foo_mode){
 	//case 0, overwrite entire file
 	case 0:
 		{
-		write_Text.open("Employees_Names.txt");
-		write_Text2.open("Employees_workDetails.txt");
-		
+			write_Text.open("Employees.txt");
+			
+			employee_number = 0;
+			//To write the very first employee, we need to have an existing parameter "1". So we write 1 into Params.txt.
+			write_Text2.open("Params.txt");
+			write_Text2 << 0;
+			write_Text2.close();
 			do{
 				std::cout << "List of Employees: " << std::endl;
 					read_Employee("Names");
 					list_Employee("Names");
-					
 					input_employeeWorkDetails();
 				system("CLS");
 				
@@ -159,7 +195,7 @@ void write_Employee(int foo_mode){
 			
 			
 		write_Text.close();
-		write_Text2.close();
+		//write_Text2.close();
 		break;
 		}
 
@@ -168,8 +204,8 @@ void write_Employee(int foo_mode){
 	//case 2, add names;
 	case 2:
 		{ //append names
-		write_Text.open("Employees_Names.txt", std::ios::app);
-		write_Text2.open("Employees_workDetails.txt", std::ios::app);
+		write_Text.open("Employees.txt", std::ios::app);
+		//write_Text2.open("Params.txt");
 		
 			do{
 				std::cout << "List of Employees: " << std::endl;
@@ -197,7 +233,7 @@ void write_Employee(int foo_mode){
 			
 			
 		write_Text.close();
-		write_Text2.close();
+		//write_Text2.close();
 		break;
 		} //Overwrite file end
 
@@ -211,33 +247,44 @@ void write_Employee(int foo_mode){
 
 
 void read_Employee(std::string foo_mode){
-	std::string input_name;
-	std::string input_workDetails;
+	std::string read_name;
+	std::string read_ID;
+	std::string read_position;
+	std::string input_salary;
+	std::string input_workTime;
+	std::string input_totalPayment;
+	std::string read_leaveStatus;
+	
+	
 	clear_vectors();
+	
 	
 	//if we only need Names, ID, Position and Hourly Salary
 	if(foo_mode=="Names"){
-		read_Text.open("Employees_Names.txt");
-		read_Text2.open("Employees_workDetails.txt");
+		read_Text.open("Employees.txt");
 		
-		while(getline(read_Text, input_name)){
-			employee_Names.push_back(input_name);
-		}
-	
-		for(int i=0; i<employee_Names.size(); i++){
-			getline(read_Text2, input_workDetails);
-				employee_ID.push_back(input_workDetails);
-			getline(read_Text2, input_workDetails);
-				employee_Position.push_back(input_workDetails);
-			getline(read_Text2, input_workDetails);
-				std::stof(input_workDetails);
-				float input_hourly = std::stof(input_workDetails);
-				employee_hourlySalaries.push_back(input_hourly);
+		
+		for(int i=0; i<employee_number; i++){
+			getline(read_Text, read_name);
+			getline(read_Text, read_ID);
+			getline(read_Text, read_position);
+			
+			getline(read_Text, input_salary);
+				float read_salary = std::stof(input_salary);
+				
+			getline(read_Text, input_workTime);
+				float read_workTime = std::stof(input_workTime);
+				
+			getline(read_Text, input_totalPayment);
+				float read_totalPayment = std::stof(input_totalPayment);
+				
+			getline(read_Text, read_leaveStatus);
+			
+			employee.emplace_back(read_name, read_ID, read_position, read_salary, read_workTime, read_totalPayment, read_leaveStatus);
 		}
 		
 		
 		read_Text.close();
-		read_Text2.close();
 	}
 	
 	//for inspecting
@@ -251,19 +298,19 @@ void read_Employee(std::string foo_mode){
 void list_Employee(std::string foo_mode){
 	
 	
-	
 	if(foo_mode=="Names"){
-		std::cout << std::left << std::setw(5) << "No." << std::setw(25) << "Name" << std::setw(15) << "ID" << std::setw(15) << "Position" << std::setw(10) << "Salary per hour" << "\n";
-		for(int i = 0; i < employee_Names.size(); i++){
+		std::cout << std::left << std::setw(5) << "No." << std::setw(20) << "Name" << std::setw(15) << "ID" << std::setw(15) << "Position";
+		std::cout << std::setw(10) << "Hourly" << std::setw(10) << "Clock-in" << std::setw(10) << "Payment" << std::setw(10) << "Leave Status" << "\n";
+		for(int i = 0; i < employee_number; i++){
 		
-			std::cout << std::left << std::setw(5) << i+1 << std::setw(25) << employee_Names[i] << std::setw(15) << employee_ID[i] << std::setw(15) << employee_Position[i] << std::setw(10) << employee_hourlySalaries[i] << std::endl;
-			
-			
+			std::cout << std::left << std::setw(5) << i+1 << std::setw(20) << employee[i].name << std::setw(15) << employee[i].ID << std::setw(15) << employee[i].position;
+			std::cout << std::setw(10) << employee[i].salary << std::setw(10) << employee[i].workTime << std::setw(10) << employee[i].totalPayment << std::setw(10) << employee[i].leaveStatus << std::endl;
+					
+					
+					
+					
 		}
 	}
-	
-	
-	
 	
 	
 	
@@ -281,10 +328,12 @@ void input_employeeWorkDetails(){
 		
 	
 		do{
+			employee_number++;
 			std::cout << "\nInput new Employee Name [input 0 to exit]: ";
 			getline(std::cin, input_textName);
 					if(input_textName=="0"){
 						flag_inputDetails=false;
+						employee_number--;
 						break;
 					}
 					
@@ -302,6 +351,7 @@ void input_employeeWorkDetails(){
 						input_textPosition = "Manager";
 					}else{
 						flag_inputDetails=false;
+						employee_number--;
 						break;
 					}
 					
@@ -311,14 +361,25 @@ void input_employeeWorkDetails(){
 			std::cin >> input_hourlySalary;
 					if(input_hourlySalary==0){
 						flag_inputDetails=false;
+						employee_number--;
 						break;
 					}
 			
 			
-			write_Text << input_textName << std::endl;
-			write_Text2 << "A23-" << generate_ID() << std::endl;
-			write_Text2 << input_textPosition << std::endl;
-			write_Text2 << input_hourlySalary << std::endl;
+			write_Text << input_textName << std::endl;			//name
+			write_Text << "A23-" << generate_ID() << std::endl;	//ID
+			write_Text << input_textPosition << std::endl;		//position
+			write_Text << input_hourlySalary << std::endl;		//salary
+			write_Text << "0" << std::endl;						//workTime
+			write_Text << "0" << std::endl;						//totalPayment
+			write_Text << "No." << std::endl;					//Leavestatus
+			
+			//parameter updating
+			write_Text2.open("Params.txt");
+			write_Text2 << employee_number;
+			write_Text2.close();
+			
+			
 			std::cin.ignore(256, '\n');
 			
 		}while(flag_inputDetails==true);
@@ -342,28 +403,20 @@ int generate_ID(){
 
 //handy function for clearing all vectors
 void clear_vectors(){
-	
-employee_ID.clear();
-employee_Names.clear();
 
-employee_hourlySalaries.clear();
-employee_totalPayment.clear();
-employee_Overtime.clear();
-employee_workTimes.clear();
-employee_leaveStatus.clear();
-employee_Position.clear();
+employee.clear();
 
 }
 
 
-
-
-/* asdasd
-			cout << left << setw(5) << "No." << setw(25) << "Name" << setw(15) << "Price" << setw(15) << "Quantity" << "\n";
-			
-			for (int i=0; i<=imax; i++){
-				cout << left << setw(5) << i+1 << setw(25) << item[i];
-				cout << fixed << setprecision(2) << setw(15) << prc[i] << setw(15) << qty[i]  << setw(15) << trtotal[i] <<"\n";
-				}
-				
-*/
+int read_params(){
+	int number;
+	read_Text2.open("Params.txt");
+	if(!read_Text2.is_open()){
+		number=0;
+	}else{
+	read_Text2 >> number;
+	}
+	read_Text2.close();
+	return number;
+}
